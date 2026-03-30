a51d9588c2121ca2561bdd7d8eeec8176e922003
Jeshua Arias <ariasjeshua2@gmail.com>
2026-03-30 10:18:47 -0600

    Folder structure and files
    Add folder structure and main files to project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      {/* nav bar */}
+      <Navbar />
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default App
