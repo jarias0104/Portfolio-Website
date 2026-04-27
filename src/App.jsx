050e8ef2e7928fd9cba85b18310b3aa83c3f6121
Jeshua Arias <ariasjeshua2@gmail.com>
2026-04-27 14:31:14 -0600

    Projects section completed
    Added a Project and ProjectDetails component, one works to get the details of each project like title, description, img etc and the other to organize them and then display them inside the parent Project.jsx file, use motion for img pop up when clicking on a project onMouseEnter to display a img preview on the project when the user hovers on top of the respective project section

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
+import Projects from "./sections/Projects";
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
-      {/* nav bar */}
       <Navbar/>
-      {/* hero */}
       <Hero/>
-      {/* about */}
       <About />
-      <section className="min-h-screen" />
-      <section className="min-h-screen" />
-      {/* projects */}
+      <Projects />
       {/* experience */}
       {/* testimonial */}
       {/* contact */}
