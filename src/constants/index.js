0ee9de11d1a311658f21ecd0c5d3bfdd703bda71
Jeshua Arias <ariasjeshua2@gmail.com>
2026-04-27 15:48:51 -0600

    Experiences completed
    Added Experiences section andd Timeline component importing from acertity ui and get data from constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -229,31 +229,20 @@ export const mySocials = [
     href: "https://www.linkedin.com/in/jeshua-arias-0b0b8232b/",
     icon: "/assets/socials/linkedIn.svg",
   },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/_epuki/",
+    icon: "/assets/socials/instagram.svg"
+  }
 ];
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "English Teacher",
+    job: "Montessori English Teacher",
+    date: "2024",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
-    ],
-  },
-  {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
-    contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+      "Complete later...",
     ],
   },
   {
@@ -261,7 +250,7 @@ export const experiences = [
     job: "Self-Employed",
     date: "2025-Present",
     contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Created a personal portfolio using React, Vite, TailWind CSS to showcase technical expertise.",
       "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
     ],
   },
